43355ccc41f732e24f27b1d18f85bd3c4eb884cc
Kelly Lampotang <klampotang@fb.com>
2018-07-14 18:49:52 -0500

    new files

---
+++ HomeAwayHack/ViewControllers/ListingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListingTableViewCell.swift
+//  HomeAwayHack
+//
+//  Created by Kelly Lampotang on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import UIKit
+
+class ListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
