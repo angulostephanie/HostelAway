9daa295ee5264d392b02a10e27e079d3533d1634
Kelly Lampotang <klampotang@fb.com>
2018-07-14 18:54:04 -0500

    book it file added

---
+++ HomeAwayHack/ViewControllers/BookItViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BookItViewController.swift
+//  HomeAwayHack
+//
+//  Created by Kelly Lampotang on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import UIKit
+
+class BookItViewController: UIViewController {
+
+    @IBOutlet weak var bookItLabel: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func bookItClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
