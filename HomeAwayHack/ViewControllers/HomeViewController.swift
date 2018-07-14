49c7e2346467be31b702aa578df17ed5eae01a94
angulostephanie <angulostephanie21@gmail.com>
2018-07-14 16:43:50 -0500

    Made basic storyboard for new app

---
+++ HomeAwayHack/ViewControllers/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  HomeAwayHack
+//
+//  Created by Stephanie Angulo on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func onDetailBtn(_ sender: Any) {
+    self.performSegue(withIdentifier: "homeToDetailSegue", sender: nil)
+  }
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HomeAwayHack/ViewControllers/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  HomeAwayHack
+//
+//  Created by Stephanie Angulo on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func onLoginBtn(_ sender: Any) {
+    self.performSegue(withIdentifier: "loginToFilterSegue", sender: nil)
+  }
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
