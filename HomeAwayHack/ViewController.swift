d40f9db16e8b6287a817e3d17ed6075c62073d51
angulostephanie <angulostephanie21@gmail.com>
2018-07-14 16:33:51 -0500

    Initial Commit

---
+++ HomeAwayHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeAwayHack
+//
+//  Created by Stephanie Angulo on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
