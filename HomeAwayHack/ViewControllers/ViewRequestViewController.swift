2db1326e8cfc02c085bdbc6c99dcaaf2d91f0311
Kelly Lampotang <klampotang@fb.com>
2018-07-15 09:29:22 -0500

    request

--- HomeAwayHack/ViewControllers/ViewRequestViewController.swift
+++ HomeAwayHack/ViewControllers/ViewRequestViewController.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 
-class ViewRequestViewController: UIViewController {
+class ViewRequestViewController: UIViewController, UITableViewDataSource {
 
+    let entries = ["Request from Bill", "Request from Ming", "Request from Stephanie", "Request from Gregory", "Request from Kelly", "Request from Pedro", "Request from Sanodval"]
     @IBOutlet weak var requestTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.requestTableView.dataSource = self
+        self.requestTableView.reloadData()
+        
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count;
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "request", for: indexPath) as! RequestTableViewCell
+        cell.requestLabel.text = entries[indexPath.row]
+        return cell
+    }
 
     /*
     // MARK: - Navigation
