75fe12b351cf538e155ec137fabf5c89900ac603
angulostephanie <angulostephanie21@gmail.com>
2018-07-14 20:29:47 -0500

    Added firebase data structures

---
+++ HomeAwayHack/Firebase/Rental.swift
@@ -0,0 +1,20 @@
+//
+//  Rental.swift
+//  HomeAwayHack
+//
+//  Created by Stephanie Angulo on 7/14/18.
+//  Copyright © 2018 Stephanie Angulo. All rights reserved.
+//
+
+import Foundation
+
+class Rental: NSObject {
+  var maxPeople: Int?
+  var currentPeople: Int?
+  var host: User?
+  var rentalID: String?
+  
+  init(maxPeople:Int?, currentPeople: Int?, host:User?, rentalID:String?) {
+    
+  }
+}
