cbd5169dde7cc3215d4396368490e3ae5a791838
angulostephanie <angulostephanie21@gmail.com>
2018-07-14 19:07:42 -0500

    google stuff works betterish

--- HomeAwayHack/ViewControllers/LoginViewController.swift
+++ HomeAwayHack/ViewControllers/LoginViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 import Firebase
 import GoogleSignIn
 
-class LoginViewController: UIViewController, GIDSignInUIDelegate {
-
+class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+  
     override func viewDidLoad() {
-        super.viewDidLoad()
-        GIDSignIn.sharedInstance().uiDelegate = self
-      
-
+      super.viewDidLoad()
+      GIDSignIn.sharedInstance().uiDelegate = self
+      GIDSignIn.sharedInstance().delegate = self
+      let buttonFrame : CGRect = CGRect.init(x: 0, y: 0, width: 100, height: 50)
+      let gdSignInButton = GIDSignInButton.init(frame:buttonFrame)
+        gdSignInButton.center = view.center
+        view.addSubview(gdSignInButton)
         // Do any additional setup after loading the view.
     }
 
@@ -25,14 +28,45 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-  @IBAction func onLoginBtn(_ sender: Any) {
-    print("signing in")
-    GIDSignIn.sharedInstance().signIn()
-
-    if GIDSignIn.sharedInstance().hasAuthInKeychain() {
-        self.performSegue(withIdentifier: "loginToFilterSegue", sender: nil)
+    @IBAction func onLoginBtn(_ sender: Any) {
+      print("signing in")
+    }
+  
+   func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+      guard error == nil else {
+          print("Error while trying to redirect : \(error)")
+          return
       }
-  }
+      print("Successful Redirection")
+    }
+
+
+    //MARK: GIDSignIn Delegate
+
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!)
+    {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+          print("User id is \(String(describing: userId))")
+
+            let idToken = user.authentication.idToken // Safe to send to the server
+          print("Authentication idToken is \(String(describing: idToken))")
+            let fullName = user.profile.name
+          print("User full name is \(String(describing: fullName))")
+            let givenName = user.profile.givenName
+          print("User given profile name is \(String(describing: givenName))")
+            let familyName = user.profile.familyName
+          print("User family name is \(String(describing: familyName))")
+            let email = user.profile.email
+          print("User email address is \(String(describing: email))")
+          self.performSegue(withIdentifier: "loginToFilterSegue", sender: nil)
+            // ...
+        } else {
+            print("ERROR ::\(error.localizedDescription)")
+        }
+    }
+
   
     /*
     // MARK: - Navigation
