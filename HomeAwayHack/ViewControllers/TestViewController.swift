f53d82eb4de9c5f2ef93ef1f20a12fe86b6c1ddc
angulostephanie <angulostephanie21@gmail.com>
2018-07-14 22:54:15 -0500

    lala

--- HomeAwayHack/ViewControllers/TestViewController.swift
+++ HomeAwayHack/ViewControllers/TestViewController.swift
@@ -2,17 +2,21 @@
 //  TestViewController.swift
 //  HomeAwayHack
 //
+
 //  Created by Stephanie Angulo on 7/14/18.
 //  Copyright © 2018 Stephanie Angulo. All rights reserved.
 //
 
 import UIKit
+import Firebase
+
 
 class TestViewController: UIViewController {
+  var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +25,13 @@ class TestViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+  @IBAction func addToFirebase(_ sender: Any) {
+//    let user = User(firstName: "stephanie", lastName: "angulo", userID: "1234567890")
+//    let test = Rental(maxPeople: 5, currentPeople: 4, host: user, rentalID: "987654321")
+//    print(user.firstName)
+//    self.ref.child("test").child("stephanie").setValue("angulo")
+  }
+  
     /*
     // MARK: - Navigation
 
